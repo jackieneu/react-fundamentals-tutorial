5ddef29b2c06556e3395b322fb4a21c7f2150964
Jacquelynn Neu <jlamb@uwalumni.com>
2023-01-29 16:40:21 -0600

    initial commit

---
+++ src/Book.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './index.css'
+
+const Book = ({ img, title, author, number }) => {
+  return (
+    <article className='book'>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <span className='number'>#{number + 1}</span>
+    </article>
+  )
+}
+
+export default Book
---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+const books = [
+  {
+    id: 1,
+    author: 'Colleen Hoover',
+    title: 'It Ends with Us',
+    img: img1,
+  },
+  {
+    id: 2,
+    author: 'Prince Harry The Duke of Sussex',
+    title: 'Spare',
+    img: img2,
+  },
+  {
+    id: 3,
+    author: 'Colleen Hoover',
+    title: 'It Starts with Us',
+    img: img3,
+  },
+]
+
+export default books
